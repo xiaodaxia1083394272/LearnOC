4aef3c742ca9d25d27300b48b8dc05599f1b72f9
xiaodaxia1083394272 <1083394272@qq.com>
2016-04-28 16:08:21 +0800

    1

--- JustForTest/JustForTest/ViewController.m
+++ JustForTest/JustForTest/ViewController.m
@@ -19,7 +19,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib
     
-    NSString *nihao = @"second";
 }
 
 - (void)didReceiveMemoryWarning {
